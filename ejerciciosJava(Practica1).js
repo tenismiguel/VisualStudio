e58c2a6e387b749675687462daa5ddc9bef46e56
Miguel <zamitmiguel0@gmail.com>
2024-05-06 20:41:45 +0200

    Hasta el 5

--- ejerciciosJava(Practica1).js
+++ ejerciciosJava(Practica1).js
@@ -1,4 +1,4 @@
-function ejer1(params) {
+function ejer1() {
     alert("Hola")
     num1 = parseInt(prompt("Dime el primer numero"))
     num2 = parseInt(prompt("Dime el segundo numero"))
@@ -11,4 +11,58 @@ function ejer1(params) {
     }else{
         document.write("El numero mas alto es: "+num3)
     }
+}
+
+function ejer2() {
+    num1 = parseInt(prompt("Dime un numero: "))
+
+    if(num1 % 2 == 0){
+        alert("El numero "+num1+" es un numero par")
+    }else{
+        alert("El numero "+num1+" es impar")
+    }
+}
+
+function ejer3() {
+    sumatorio = 0;
+    for(i = 1; i <= 100; i++){
+        sumatorio = sumatorio + i;
+    }
+    alert("El resultado de sumar los 100 primeros numeros es: "+sumatorio)
+}
+
+function ejer4() {
+    sumatorio = 0;
+    num = parseInt(prompt("Dime un numero: "));
+    for(i = num; i <= num+100; i++){
+        sumatorio = sumatorio + i;
+    }
+    alert("El resultado de sumar los 100 siguientes numeros es: "+sumatorio)
+}
+
+function ejer5() {
+    num1 = parseInt(prompt("Dime un numero: "));
+    num2 = parseInt(prompt("Dime otro numero: "));
+    num3 = parseInt(prompt("Dime el ultimo numero: "));
+
+    var max;
+    var min;
+
+    if(num1 > num2 && num1 > num3){
+        max = num1;
+    }else if(num2 > num1 && num2 > num3){
+        max = num2;
+    }else{
+        max = num3;
+    }
+
+    if(num1 < num2 && num1 < num3){
+        min = num1
+    }else if(num2 < num1 && num2 < num3){
+        min = num2;
+    }else{
+        min = num3;
+    }
+
+    alert("El numero mayor es: "+max+"\nY el numero menor es "+min)
 }
